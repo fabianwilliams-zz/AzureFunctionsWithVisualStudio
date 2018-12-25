96374ec8d68913deae69a276a394538d9e07f686
Fabian Williams <fabian@adotob.com>
2018-12-24 22:30:25 -0500

    http Functions created for generic and to write to CosmosDB done and working

---
+++ FunctWithFabs/FunctWithFabs/Models.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Newtonsoft.Json;
+
+
+public partial class ColleagueAccolade
+{
+    [JsonProperty("id")]
+    public string Id { get; set; } = Guid.NewGuid().ToString("n");
+
+    [JsonProperty("colleagueName")]
+    public string ColleagueName { get; set; }
+
+    [JsonProperty("colleagueUPNEmail")]
+    public string ColleagueUPNEmail { get; set; }
+
+    [JsonProperty("accoladeGivenDate")]
+    public string AccoladeGivenDate { get; set; } = DateTime.Now.ToString();
+
+    [JsonProperty("accoladeStatement")]
+    public string AccoladeStatement { get; set; }
+
+    [JsonProperty("myName")]
+    public string MyName { get; set; }
+
+    [JsonProperty("myUPNEmail")]
+    public string MyUPNEmail { get; set; }
+
+    [JsonProperty("accoladeLifeValidStatus")]
+    public bool AccoladeLifeValidStatus { get; set; } = true;
+
+    [JsonProperty("workBucksWorthy")]
+    public bool WorkBucksWorthy { get; set; }
+
+    [JsonProperty("workBucksAmount")]
+    public string WorkBucksAmount { get; set; }
+
+    [JsonProperty("workBucksId")]
+    public string WorkBucksId { get; set; }
+
+    [JsonProperty("officeLocation")]
+    public string OfficeLocation { get; set; }
+}
+public partial class FabsSession
+{
+    [JsonProperty("id")]
+    public string Id { get; set; } = Guid.NewGuid().ToString("n");
+
+    [JsonProperty("sessionNumber")]
+    public string SessionNumber { get; set; }
+
+    [JsonProperty("sessionName")]
+    public string SessionName { get; set; }
+
+    [JsonProperty("sessionDate")]
+    public string SessionDate { get; set; }
+
+    [JsonProperty("sessionCity")]
+    public string SessionCity { get; set; }
+
+    [JsonProperty("sessionRegionState")]
+    public string SessionRegionState { get; set; }
+
+    [JsonProperty("sessionCountry")]
+    public string SessionCountry { get; set; }
+
+    [JsonProperty("review")]
+    public ReviewOfSession[] Review { get; set; }
+}
+
+public partial class ReviewOfSession
+{
+    [JsonProperty("id")]
+    public string Id { get; set; } = Guid.NewGuid().ToString("n");
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("zerotofiveRating")]
+    public long ZerotofiveRating { get; set; }
+
+    [JsonProperty("feedback")]
+    public string Feedback { get; set; }
+
+    [JsonProperty("requestContact")]
+    public bool RequestContact { get; set; }
+
+    [JsonProperty("email")]
+    public string Email { get; set; }
+}
+
+public class CreateSession
+{
+    [JsonProperty("sessionNumber")]
+    public string SessionNumber { get; set; }
+
+    [JsonProperty("sessionName")]
+    public string SessionName { get; set; }
+
+    [JsonProperty("sessionDate")]
+    public string SessionDate { get; set; }
+
+    [JsonProperty("sessionCity")]
+    public string SessionCity { get; set; }
+
+    [JsonProperty("sessionRegionState")]
+    public string SessionRegionState { get; set; }
+
+    [JsonProperty("sessionCountry")]
+    public string SessionCountry { get; set; }
+
+    [JsonProperty("review")]
+    public ReviewOfSession[] Review { get; set; }
+}
---
+++ FunctWithFabs/FunctWithFabs/httpCosmosDb/httpCosmosCrud.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace FunctWithFabs.httpCosmosDb
+{
+    public static class HttpCosmosCrud
+    {
+        [FunctionName("CreateAccolade")]
+        public static async Task<IActionResult> CreateAccolade([HttpTrigger(AuthorizationLevel.Function, "post",
+            Route = "session")] HttpRequest req,
+            [CosmosDB(
+            databaseName: "FunctWithFabsDb",
+            collectionName: "Accolades",
+            ConnectionStringSetting = "FunctWithFabsCosmosDBConn")] IAsyncCollector<ColleagueAccolade> createAccoladeOut,
+            TraceWriter log)
+        {
+            log.Info("Entry into the Create Function has occured...");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            ColleagueAccolade input = JsonConvert.DeserializeObject<ColleagueAccolade>(requestBody);
+
+            log.Info($"Input Payload is: {input}");
+
+            var acc = new ColleagueAccolade()
+            {
+                ColleagueName = input.ColleagueName,
+                ColleagueUPNEmail = input.ColleagueUPNEmail,
+                AccoladeStatement = input.AccoladeStatement,
+                MyName = input.MyName,
+                MyUPNEmail = input.MyUPNEmail,
+                AccoladeLifeValidStatus = input.AccoladeLifeValidStatus,
+                WorkBucksWorthy = input.WorkBucksWorthy,
+                WorkBucksAmount = input.WorkBucksAmount,
+                WorkBucksId = input.WorkBucksId,
+                OfficeLocation = input.OfficeLocation
+            };
+            await createAccoladeOut.AddAsync(acc);
+
+            log.Info($"Session Record Added... Work is Finished");
+
+            return acc != null
+                ? (ActionResult)new OkObjectResult(acc)
+                : new BadRequestObjectResult("Please pass a valid Accolade JSON Payload in the request body");
+        }
+    }
+}
