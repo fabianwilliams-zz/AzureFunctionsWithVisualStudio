8d9eb3e82758415d7f637acfe86483d7b5a704d5
Fabian Williams <fabian@adotob.com>
2018-12-27 22:35:39 -0500

    WIP on Text analysis Azure Funct

---
+++ FunctWithFabs/FunctWithFabs/httpText/WhatDidYouSayAboutMe.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctWithFabs.httpText
+{
+    public class IncomingText
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("language")]
+        public string Language { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+    public static class WhatDidYouSayAboutMe
+    {
+        [FunctionName("WhatDidYouSay")]
+        public static async Task<IActionResult> WhatDidYouSay(
+            [HttpTrigger(AuthorizationLevel.Function, "post", 
+            Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
